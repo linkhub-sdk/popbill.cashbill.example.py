9396b5c350813ead424ab209587eb35d8a420916
JohnJeong123 <frenchofkiss@gmail.com>
2017-08-17 14:04:37 +0900

    added Cashbill API - RevokeRegistIssue / Closedown API - typeDate Field

---
+++ revokeRegistIssue.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+imp.reload(sys)
+try: sys.setdefaultencoding('UTF8')
+except Exception as E: pass
+
+import testValue
+
+from popbill import Cashbill, CashbillService, PopbillException
+
+cashbillService = CashbillService(testValue.LinkID, testValue.SecretKey)
+cashbillService.IsTest = testValue.IsTest
+
+'''
+1건의 취소현금영수증을 즉시발행합니다.
+- 발행일 기준 오후 5시 이전에 발행된 현금영수증은 다음날 오후 2시에 국세청 전송결과를 확인할 수 있습니다.
+- 현금영수증 국세청 전송 정책에 대한 정보는 "[현금영수증 API 연동매뉴얼] > 1.4. 국세청 전송정책"을
+  참조하시기 바랍니다.
+- 취소현금영수증 작성방법 안내 - http://blog.linkhub.co.kr/702
+'''
+
+try:
+    print("=" * 15 + " 취소현금영수증 1건 즉시발행 "+ "=" * 15)
+
+    # 팝빌회원 사업자번호
+    CorpNum = testValue.testCorpNum
+
+    # 팝빌회원 아이디
+    UserID = testValue.testUserID
+
+    # 문서관리번호, 1~24자리, 영문,숫자,-,_ 조합으로 사업자별로 중복되지 않도록 구성
+    mgtKey = "20170817-42"
+
+    # 원본현금영수증 국세청승인번호, 문서정보확인(GetInfo API)로 확인가능
+    orgConfirmNum = "820116333"
+
+    # 원본현금영수증 거래일자, 문서정보확인(GetInfo API)로 확인가능
+    orgTradeDate = "20170711"
+
+    # 발행안내문자 전송여부
+    smssendYN = False
+
+    # 즉시발행 메모
+    memo = "현금영수증 즉시발행 메모"
+
+    result = cashbillService.revokeRegistIssue(CorpNum, mgtKey, orgConfirmNum, orgTradeDate, smssendYN, memo, UserID)
+
+    print("처리결과 : [%d] %s" % (result.code,result.message))
+
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code , PE.message))
