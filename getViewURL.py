3003aa5f25c9ce00d78a92ab1a7cf38b9af64792
1jae <wjkim@linkhub.co.kr>
2021-04-29 12:19:40 +0900

    added getViewURL API

---
+++ getViewURL.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+
+imp.reload(sys)
+try:
+    sys.setdefaultencoding('UTF8')
+except Exception as E:
+    pass
+
+import testValue
+
+from popbill import CashbillService, PopbillException
+
+cashbillService = CashbillService(testValue.LinkID, testValue.SecretKey)
+cashbillService.IsTest = testValue.IsTest
+cashbillService.IPRestrictOnOff = testValue.IPRestrictOnOff
+
+'''
+1건의 현금영수증 보기 팝업 URL을 반환합니다. (메뉴/버튼 제외)
+- 보안정책으로 인해 반환된 URL의 유효시간은 30초입니다.
+- https://docs.popbill.com/cashbill/python/api#GetViewURLL
+'''
+
+try:
+    print("=" * 15 + " 현금영수증 보기 팝업 URL " + "=" * 15)
+
+    # 팝빌회원 사업자번호
+    CorpNum = testValue.testCorpNum
+
+    # 현금영수증 문서번호
+    MgtKey = "20210416-01"
+
+    url = cashbillService.getViewURL(CorpNum, MgtKey)
+
+    print("URL: %s" % url)
+
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code, PE.message))
