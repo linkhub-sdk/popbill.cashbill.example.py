a54e1c8f38a421d68d3d024cd17e2c7d0258890b
1jae <wjkim@linkhub.co.kr>
2021-04-30 11:45:31 +0900

    fixed comment

--- getPaymentURL.py
+++ getPaymentURL.py
@@ -20,7 +20,7 @@ cashbillService.UseStaticIP = testValue.UseStaticIP
 cashbillService.UseLocalTimeYN = testValue.UseLocalTimeYN
 
 '''
-파트너 포인트충전 팝업 URL을 반환합니다.
+팝빌 연동회원 포인트 결재내역 URL을 반환합니다.
 - 보안정책에 따라 반환된 URL은 30초의 유효시간을 갖습니다.
 - https://docs.popbill.com/cashbill/python/api#GetPaymentURL
 '''
