9cc51a3ae405e950a590557b1f29e3e32442ca99
ZIZON97 <wlgjs101@gmail.com>
2022-08-04 14:11:03 +0900

    주석 수정.

--- getPDFURL.py
+++ getPDFURL.py
@@ -22,6 +22,7 @@ cashbillService.UseLocalTimeYN = testValue.UseLocalTimeYN
 '''
 1건의 현금영수증 PDF 다운로드 URL을 반환합니다.
 - 반환되는 URL은 보안 정책상 30초 동안 유효하며, 시간을 초과한 후에는 해당 URL을 통한 페이지 접근이 불가합니다.
+- https://docs.popbill.com/cashbill/python/api#GetPDFURL
 '''
 
 try:
@@ -31,7 +32,7 @@ try:
     CorpNum = testValue.testCorpNum
 
     # 현금영수증 문서번호
-    MgtKey = "20210429-01"
+    MgtKey = "20220803-01"
 
     url = cashbillService.getPDFURL(CorpNum, MgtKey)
 
