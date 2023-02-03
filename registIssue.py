0a19dbebb254d6e734a04432d336c13292407f97
박지헌 <jhpark@linkhubcorp.com>
2023-02-03 13:59:29 +0900

    주석 링크 변경

--- registIssue.py
+++ registIssue.py
@@ -21,7 +21,7 @@ cashbillService.UseLocalTimeYN = testValue.UseLocalTimeYN
 
 '''
 작성된 현금영수증 데이터를 팝빌에 저장과 동시에 발행하여 "발행완료" 상태로 처리합니다.
-- 현금영수증 국세청 전송 정책 : https://developers.popbill.com/guide/cashbill/$2/introduction/policy-of-send-to-nts
+- 현금영수증 국세청 전송 정책 : https://developers.popbill.com/guide/cashbill/python/introduction/policy-of-send-to-nts
 - https://developers.popbill.com/reference/cashbill/python/api/issue#RegistIssue
 '''
 
