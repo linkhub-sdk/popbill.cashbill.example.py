87d366d28170f969e5ec3e6c0ec3023ca80e63f3
정우석 <wsj@linkhubcorp.com>
2024-02-26 21:48:13 +0900

    주석(용어) 수정

--- assignMgtKey.py
+++ assignMgtKey.py
@@ -20,7 +20,7 @@ cashbillService.UseStaticIP = testValue.UseStaticIP
 cashbillService.UseLocalTimeYN = testValue.UseLocalTimeYN
 
 """
-팝빌 사이트를 통해 발행하여 문서번호가 부여되지 않은 현금영수증에 문서번호를 할당합니다.
+팝빌 사이트를 통해 발행하여 문서번호가 할당되지 않은 현금영수증에 문서번호를 할당합니다.
 - https://developers.popbill.com/reference/cashbill/python/api/etc#AssignMgtKey
 """
 
