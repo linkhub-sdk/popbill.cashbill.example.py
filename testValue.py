e4e8624d43274a06f96c98dd82060923d6551f59
정우석 <wsj@linkhubcorp.com>
2024-02-27 09:30:03 +0900

    업데이트 일자 추가

--- testValue.py
+++ testValue.py
@@ -4,6 +4,7 @@
 팝빌 현금영수증 API Python SDK Example
 
 Python 연동 튜토리얼 안내 : https://developers.popbill.com/guide/cashbill/python/getting-started/tutorial
+업데이트 일자 : 2024-02-27
 연동 기술지원 연락처 : 1600-9854
 연동 기술지원 이메일 : code@linkhubcorp.com
 
